8fd62100cc9665e9fda4d0b7d2383b63667f9712
Ilhamsyah Putra <ilhamsyah.putraa1997@gmail.com>
2023-05-25 21:11:23 +0700

    Learning

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 const http = require("node:http");
-const testModule = require('./testModule')
-const faker = require("faker")
+const testModule = require('./testModule');
+const faker = require("faker");
 
-const randomName = faker.name.firstName()
+const randomName = faker.name.firstName();
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
 const server = http.createServer((req, res) => {
+  const url = req.url
   res.statusCode = 200;
   res.setHeader("Content-Type", "text/plain");
-  res.end(randomName);
+
+  if (url === '/users') {
+    res.end("hello user")
+  } else {
+    res.end(randomName);
+  }
 });
 
 server.listen(port, hostname, () => {
